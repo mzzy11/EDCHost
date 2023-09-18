fc227db794beff652c72a71d3e9b511ff1187607
mzzy11 <143632490+mzzy11@users.noreply.github.com>
2023-09-18 18:19:00 +0800

    Update Player.cs

--- src/EdcHost/Games/Player.cs
+++ src/EdcHost/Games/Player.cs
@@ -10,10 +10,10 @@ public class Player : IPlayer
     public IPosition<float> PlayerPosition { get; private set; }
     public int WoolCount { get; private set; }
 
-    public int Health { get; private set; } // Health property
-    public int MaxHealth { get; private set; } // Max health property
-    public int Strength { get; private set; } // Strength property
-    public int ActionPoints { get; private set; } // Action points property
+    public int Health { get; private set; } /// Health property
+    public int MaxHealth { get; private set; } /// Max health property
+    public int Strength { get; private set; } /// Strength property
+    public int ActionPoints { get; private set; } /// Action points property
 
     public event EventHandler<PlayerMoveEventArgs> OnMove = delegate { };
     public event EventHandler<PlayerAttackEventArgs> OnAttack = delegate { };
@@ -22,29 +22,30 @@ public class Player : IPlayer
 
     public void Move(float newX, float newY)
     {
-        // Update the player's position information
+        /// Update the player's position information
         PlayerPosition.X = newX;
         PlayerPosition.Y = newY;
-        // Trigger the OnMove event to notify other parts that the player has moved
+        /// Trigger the OnMove event to notify other parts that the player has moved
         OnMove?.Invoke(this, new PlayerMoveEventArgs(this, PlayerPosition, new Position<float>(newX, newY)));
-    } 
+    }
+
     public void Attack(float newX, float newY)
     {
-        // Trigger the OnAttack event to notify the attacked block
+        /// Trigger the OnAttack event to notify the attacked block
         OnAttack?.Invoke(this, new PlayerAttackEventArgs(this, Strength, new Position<float>(newX, newY)));
-    } 
+    }
+    
     public void Place(float newX, float newY)
     {
-        // Check if the player has wool in their inventory, and if so, process wool data
-        // Trigger the OnPlace event to notify the placed block
+        /// Check if the player has wool in their inventory, and if so, process wool data.
+        ///  Trigger the OnPlace event to notify the placed block
         if(WoolCount > 0){
-            WoolCount -= 1;
             OnPlace?.Invoke(this, new PlayerPlaceEventArgs(this, new Position<float>(newX, newY)));
         }
-    } 
+    }
     public void Hurt(int EnemyStrength)
     {
-        // Implement the logic for being hurt
+        /// Implement the logic for being hurt
         if(Health > EnemyStrength){
             Health -= EnemyStrength;
         }
@@ -53,7 +54,7 @@ public class Player : IPlayer
             IsAlive = false;
             OnDie?.Invoke(this, new PlayerDieEventArgs(this));
         }
-    } 
+    }
     public Player()
     {
         PlayerId = 1;
@@ -61,16 +62,16 @@ public class Player : IPlayer
         IsAlive = true;
         HasBed = true;
 
-        // Initialize SpawnPoint
-        SpawnPoint = new Position<float>(0, 0); // Initial coordinates can be set as needed
-        PlayerPosition = new Position<float>(0, 0); // Initial coordinates can be set as needed
+        /// Initialize SpawnPoint
+        SpawnPoint = new Position<float>(0, 0); /// Initial coordinates can be set as needed
+        PlayerPosition = new Position<float>(0, 0); /// Initial coordinates can be set as needed
         WoolCount = 0;
 
-        // Initialize player attributes
-        Health = 20; // Initial health
-        MaxHealth = 20; // Initial max health
-        Strength = 1; // Initial strength
-        ActionPoints = 1; // Initial action points
+        /// Initialize player attributes
+        Health = 20; /// Initial health
+        MaxHealth = 20; /// Initial max health
+        Strength = 1; /// Initial strength
+        ActionPoints = 1; /// Initial action points
     }
     public Player(int id, float initialX, float initialY, float initialX2, float initialY2)
     {
@@ -82,10 +83,10 @@ public class Player : IPlayer
         PlayerPosition = new Position<float>(initialX2, initialY2);
         WoolCount = 0;
 
-        Health = 25; // Initial health
-        MaxHealth = 25; // Initial max health
-        Strength = 1; // Initial strength
-        ActionPoints = 1; // Initial action points
+        Health = 25; /// Initial health
+        MaxHealth = 25; /// Initial max health
+        Strength = 1; /// Initial strength
+        ActionPoints = 1; /// Initial action points
         ActionPoints = 1;
     }
     public void PerformActionPosition(IPlayer.ActionKindType actionKind, float X, float Y)
@@ -93,21 +94,18 @@ public class Player : IPlayer
         switch (actionKind)
         {
             case IPlayer.ActionKindType.Attack:
-                // Implement the logic for attacking
+                /// Implement the logic for attacking
                 Attack(X, Y);
                 break;
             case IPlayer.ActionKindType.PlaceBlock:
-                // Implement the logic for placing a block
+                /// Implement the logic for placing a block
                 Place(X, Y);
                 break;
             default:
-                // Handle unknown action types
+                /// Handle unknown action types
                 break;
         }
     }
-    
-   
-
     public bool Trade(IPlayer.CommodityKindType commodityKind)
     {
         switch (commodityKind)
@@ -125,8 +123,9 @@ public class Player : IPlayer
                     EmeraldCount -= 20 - MaxHealth;
                     MaxHealth += 1;
                     return true;
-                }// Implement the logic for health boost
-                // You can perform the health boost operation here and update the player's status
+                }
+                /// Implement the logic for health boost
+                /// You can perform the health boost operation here and update the player's status
                 break;
             case IPlayer.CommodityKindType.HealthPotion:
                 if(EmeraldCount >= 4 && Health < MaxHealth){
@@ -134,8 +133,8 @@ public class Player : IPlayer
                     Health += 1;
                     return true;
                 }
-                // Implement the logic for health potion
-                // You can perform the health potion use operation here and update the player's status
+                /// Implement the logic for health potion
+                /// You can perform the health potion use operation here and update the player's status
                 break;
             case IPlayer.CommodityKindType.StrengthBoost:
                 if(EmeraldCount >= Math.Pow(2,ActionPoints)){
@@ -143,8 +142,8 @@ public class Player : IPlayer
                     Strength += 1;
                     return true;
                 }
-                // Implement the logic for strength boost
-                // You can perform the strength boost operation here and update the player's status
+                /// Implement the logic for strength boost
+                /// You can perform the strength boost operation here and update the player's status
                 break;
             case IPlayer.CommodityKindType.Wool:
                 if(EmeraldCount >= 1){
@@ -154,11 +153,9 @@ public class Player : IPlayer
                 }
                 break;
             default:
-                // Handle unknown commodity types
+                /// Handle unknown commodity types
                 break;
         }
         return false;
     }
-
-    // You can add other game logic methods and event handling here
 }
