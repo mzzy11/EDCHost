0013258036f7ed4de269f794c7d7aecb0d6e0686
mzzy11 <143632490+mzzy11@users.noreply.github.com>
2023-09-18 18:23:31 +0800

    Update PlayerPlaceEventArgs.cs

--- src/EdcHost/Games/EventArgs/PlayerPlaceEventArgs.cs
+++ src/EdcHost/Games/EventArgs/PlayerPlaceEventArgs.cs
@@ -8,7 +8,7 @@ public class PlayerPlaceEventArgs : EventArgs
     public IPlayer Player { get; }
 
     /// <summary>
-    /// The position of the player after the movement.
+    /// The position the player place a block.
     /// </summary>
     public IPosition<float> Position { get; }
 
