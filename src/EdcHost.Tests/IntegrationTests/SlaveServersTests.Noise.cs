fa3df3fb2ca9ff6ca7d1b8c172658bce15a574b1
Zijian Zhang <futrime@outlook.com>
2023-10-28 20:36:15 +0800

    test: add random COM name test

--- src/EdcHost.Tests/IntegrationTests/SlaveServersTests.Noise.cs
+++ src/EdcHost.Tests/IntegrationTests/SlaveServersTests.Noise.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using EdcHost.SlaveServers;
 using Xunit;
 
@@ -18,27 +19,27 @@ public partial class SlaveServersTests
     [InlineData(19198)]
     [InlineData(191981)]
     [InlineData(1919810)]
-    public void Noise(int randomSeed)
+    public void Random(int randomSeed)
     {
-        const string PortName = "COM1";
         const int MaxLength = 100000000;
         const int SlaveToHostBytesCount = 7;
         const int SlaveToHostDataBytesCount = 2;
 
         // Arrange
-        SerialPortWrapperMock serialPortWrapperMock = new(PortName);
+        Random random = new(randomSeed);
+        string portName = Encoding.ASCII.GetString(GenerateRandomBytes(random, random.Next(0, MaxLength)));
+        SerialPortWrapperMock serialPortWrapperMock = new(portName);
         SerialPortHubMock serialPortHubMock = new()
         {
             SerialPorts = {
-                { PortName, serialPortWrapperMock }
+                { portName, serialPortWrapperMock }
             }
         };
-        Random random = new(randomSeed);
         ISlaveServer slaveServer = new SlaveServer(serialPortHubMock);
 
         // Act
         slaveServer.Start();
-        slaveServer.OpenPort(PortName, 0);
+        slaveServer.OpenPort(portName, 0);
 
         Assert.True(serialPortWrapperMock.IsOpen);
 
